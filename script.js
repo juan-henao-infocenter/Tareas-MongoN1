205833cc2a1c71ac6b55bbdbddecb7ee037915b2
juanphenaor <144962419+juan-henao-infocenter@users.noreply.github.com>
2023-09-29 18:25:09 -0500

    shopping-cart

--- script.js
+++ script.js
@@ -8,26 +8,42 @@ Promise.all([promise1, promise2])
 
     console.log(list1, list2)
 
-    const cartItems = list1.concat(list2);
+    const instruments = list1.concat(list2);
     let arregloSinRepetidos = []
 
-    cartItems.forEach(function (elemento) {
+    instruments.forEach(function (elemento) {
       if (arregloSinRepetidos.every((e) => e.id != elemento.id)) {
         arregloSinRepetidos.push(elemento);
       }
     });
 
-    console.log(cartItems);
+    console.log(instruments);
+ 
+    // Escuchar el clic en el botón "Agregar al carrito" en cada tarjeta
+    document.addEventListener('click', (event) => {
+      if (event.target.classList.contains('add-to-cart')) {
+        const card = event.target.closest('.card');
+        const instrumentId = card.dataset.instrumentId;
+        const instrument = arregloSinRepetidos.find(
+          (item) => item.id === instrumentId
+        );
+        if (instrument) {
+          addToCart(instrument);
+        }
+      }
+    });
+
     // Función para generar una tarjeta para un instrumento
     function createInstrumentCard(instrument) {
       return `
         <div class="col">
-          <div class="card h-100">
+          <div class="card h-100" data-instrument-id="${instrument.id}">
             <img src="${instrument.image}" alt="${instrument.name}" class="card-img-top w-100" style="max-height: 200px;">
             <div class="card-body">
               <h5 class="card-title">${instrument.name}</h5>
               <p class="card-text">Tipo: ${instrument.type}</p>
               <p class="card-text">Precio: $${instrument.price}</p>
+              <button class="btn btn-primary add-to-cart">Agregar al carrito</button>
               <!-- Agregar más detalles aquí, como el botón "Agregar al carrito" -->
             </div>
           </div>
@@ -43,7 +59,7 @@ Promise.all([promise1, promise2])
         instrumentList.innerHTML += cardHTML;
       });
     }
-    
+
     renderInstrumentList();
   })
 
