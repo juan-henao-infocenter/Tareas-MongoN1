fbdcb477c1788efadc7b92c653ff82ccc733dc5d
juanphenaor <144962419+juan-henao-infocenter@users.noreply.github.com>
2023-09-28 19:24:30 -0500

    Cambios

--- script.js
+++ script.js
@@ -1,130 +1,130 @@
-const cartItems =[
-    {
-      "name": "Acoustic Guitar",
-      "type": "String Instrument",
-      "image": "https://example.com/acoustic_guitar.jpg",
-      "id": "abc123",
-      "price": 500,
-      "retail_price": 800,
-      "quantity":5
-    },
-    {
-      "name": "Electric Guitar",
-      "type": "String Instrument",
-      "image": "https://example.com/electric_guitar.jpg",
-      "id": "def456",
-      "price": 700,
-      "retail_price": 1200,
-      "quantity":2
-    },
-    {
-      "name": "Drums",
-      "type": "Percussion Instrument",
-      "image": "https://example.com/drums.jpg",
-      "id": "ghi789",
-      "price": 1000,
-      "retail_price": 1500,
-      "quantity":1
-    },
-    {
-      "name": "Piano",
-      "type": "Keyboard Instrument",
-      "image": "https://example.com/piano.jpg",
-      "id": "jkl012",
-      "price": 2000,
-      "retail_price": 3000,
-      "quantity":1
-    },
-    {
-      "name": "Violin",
-      "type": "String Instrument",
-      "image": "https://example.com/violin.jpg",
-      "id": "mno345",
-      "price": 300,
-      "retail_price": 500,
-      "quantity":5
-    },
-    {
-      "name": "Trumpet",
-      "type": "Brass Instrument",
-      "image": "https://example.com/trumpet.jpg",
-      "id": "pqr678",
-      "price": 400,
-      "retail_price": 700,
-      "quantity":3
-    },
-    {
-      "name": "Flute",
-      "type": "Woodwind Instrument",
-      "image": "https://example.com/flute.jpg",
-      "id": "stu901",
-      "price": 200,
-      "retail_price": 400,
-      "quantity":1
-    },
-    {
-      "name": "Saxophone",
-      "type": "Woodwind Instrument",
-      "image": "https://example.com/saxophone.jpg",
-      "id": "vwx234",
-      "price": 800,
-      "retail_price": 1300,
-      "quantity":2
-    },
-    {
-      "name": "Clarinet",
-      "type": "Woodwind Instrument",
-      "image": "https://example.com/clarinet.jpg",
-      "id": "yz0123",
-      "price": 400,
-      "retail_price": 700,
-      "quantity":2
-    },
-    {
-      "name": "Bass Guitar",
-      "type": "String Instrument",
-      "image": "https://example.com/bass_guitar.jpg",
-      "id": "456789",
-      "price": 600,
-      "retail_price": 1000,
-      "quantity":3
-    }
-  ]
+const cartItems = [
+  {
+    name: "Guitarra Acústica",
+    type: "Instrumento de Cuerdas",
+    image: "https://media.istockphoto.com/id/156547833/es/foto/guitarra-ac%C3%BAstica.jpg?s=1024x1024&w=is&k=20&c=nmJYjciohJ-Z7OiyGjOf91bBeV9kQyObZlomf_n_KEg=",
+    id: "abc123",
+    price: 500,
+    retail_price: 800,
+    quantity: 5,
+  },
+  {
+    name: "Guitarra Eléctrica",
+    type: "Instrumento de Cuerdas",
+    image:
+      "https://cdn.pixabay.com/photo/2017/08/06/07/28/guitar-2589863_1280.jpg",
+    id: "def456",
+    price: 700,
+    retail_price: 1200,
+    quantity: 2,
+  },
+  {
+    name: "Batería",
+    type: "Instrumento de Percusión",
+    image: "https://cdn.pixabay.com/photo/2023/01/29/00/16/drums-7751985_1280.jpg",
+    id: "ghi789",
+    price: 1000,
+    retail_price: 1500,
+    quantity: 1,
+  },
+  {
+    name: "Piano",
+    type: "Instrumento de Teclado",
+    image: "https://cdn.pixabay.com/photo/2017/04/04/20/40/instrument-2203044_1280.jpg",
+    id: "jkl012",
+    price: 2000,
+    retail_price: 3000,
+    quantity: 1,
+  },
+  {
+    name: "Violín",
+    type: "Instrumento de Cuerdas",
+    image: "https://cdn.pixabay.com/photo/2017/07/14/21/57/instrument-2505099_1280.jpg",
+    id: "mno345",
+    price: 300,
+    retail_price: 500,
+    quantity: 5,
+  },
+  {
+    name: "Trompeta",
+    type: "Instrumento de Viento Metal",
+    image: "https://cdn.pixabay.com/photo/2011/06/17/15/14/trumpet-7975_1280.jpg",
+    id: "pqr678",
+    price: 400,
+    retail_price: 700,
+    quantity: 3,
+  },
+  {
+    name: "Flauta",
+    type: "Instrumento de Viento Madera",
+    image: "https://cdn.pixabay.com/photo/2020/04/16/13/11/antique-5050542_1280.jpg",
+    id: "stu901",
+    price: 200,
+    retail_price: 400,
+    quantity: 1,
+  },
+  {
+    name: "Saxofón",
+    type: "Instrumento de Viento Madera",
+    image: "https://cdn.pixabay.com/photo/2016/05/24/11/54/saxophone-1412218_1280.jpg",
+    id: "vwx234",
+    price: 800,
+    retail_price: 1300,
+    quantity: 2,
+  },
+  {
+    name: "Clarinete",
+    type: "Instrumento de Viento Madera",
+    image: "https://cdn.pixabay.com/photo/2016/10/16/04/51/music-1744419_1280.jpg",
+    id: "yz0123",
+    price: 400,
+    retail_price: 700,
+    quantity: 2,
+  },
+  {
+    name: "Bajo Eléctrico",
+    type: "Instrumento de Cuerdas",
+    image: "https://cdn.pixabay.com/photo/2021/06/10/12/22/bass-guitar-6325873_1280.jpg",
+    id: "456789",
+    price: 600,
+    retail_price: 1000,
+    quantity: 3,
+  },
+];
 
-  const cartContainer = document.getElementById("cart-container");
 
-    // Itera a través de los elementos del carrito y crea el contenido HTML
-    cartItems.forEach((item) => {
-      const itemDiv = document.createElement("div");
-      itemDiv.className = "cart-item";
+// Obtén una referencia a la lista donde se mostrarán los elementos del carrito
+const cartList = document.getElementById("cart-container");
+// Obtén una referencia al elemento donde se mostrará el precio total de todos los productos
+const totalPriceElement = document.getElementById("total-price");
 
-      const itemName = document.createElement("h2");
-      itemName.textContent = item.name;
+// Utiliza map() para crear una lista de elementos del carrito
+const cartItemsHTML = cartItems.map((item) => {
+  const unitTotalPrice = item.price * item.quantity;
 
-      const itemPrice = document.createElement("p");
-      itemPrice.textContent = `Precio: $${item.price}`;
+  return `
+    <li class="list-group-item">
+      <div class="d-flex flex-row justify-content-start">
+        
+        <div class="col-md-2">
+          <img src="${item.image}" alt="${item.name}" class="img-fluid" style="height: 100px;"/>
+        </div>
 
-      const itemQuantity = document.createElement("p");
-      itemQuantity.textContent = `Cantidad: ${item.quantity}`;
+        <div class="col-md-10 d-flex flex-row justify-content-between">
+          <h5>${item.name}</h5>
+          <p>Precio: $${item.price}</p>
+          <p>Precio Total: $${unitTotalPrice}</p>
+          <p>Cantidad: ${item.quantity}</p>
+        </div>
 
-      const itemImage = document.createElement("img");
-      itemImage.src = item.image;
-      itemImage.alt = item.name;
+      </div>
+    </li>
+  `;
+});
 
-      itemDiv.appendChild(itemName);
-      itemDiv.appendChild(itemPrice);
-      itemDiv.appendChild(itemQuantity);
-      itemDiv.appendChild(itemImage);
+// Agrega la lista generada al contenedor de la lista del carrito
+cartList.innerHTML = cartItemsHTML.join('');
 
-      cartContainer.appendChild(itemDiv);
-    });
-
-    
-    const totalPrice = cartItems.reduce(function(total, instrument) {
-      return total + (instrument.retail_price*instrument.quantity);
-    },0);
-
-    const priceContainer = document.getElementById("total-price");
-    const itemTotalPrice = document.createElement("h2");
-    itemTotalPrice.textContent = totalPrice;
-    priceContainer.appendChild(itemTotalPrice);
+// Calcula y muestra el precio total de todos los productos
+const totalPrice = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+totalPriceElement.textContent = `$${totalPrice}`;
