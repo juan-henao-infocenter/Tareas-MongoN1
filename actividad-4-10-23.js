c6f865b447c7c61da5650cd7a7185190c2ff4aac
juanphenaor <144962419+juan-henao-infocenter@users.noreply.github.com>
2023-10-04 20:54:32 -0500

    puntos hasta 4

--- actividad-4-10-23.js
+++ actividad-4-10-23.js
@@ -1,54 +1,65 @@
-getNestedRecordsExample = () => [
+const getNestedRecordsExample = () => [
     {
-      id: 1,
-      nombre: 'Camila Lopez',
-      detalles: {
-        descripcion: 'matricula normal',
-        contacto: {
-          email: 'clopez@mail.com',
-		  telefono: 83242834
+        id: 1,
+        nombre: 'Camila Lopez',
+        detalles: {
+            descripcion: 'matricula normal',
+            contacto: {
+                email: 'clopez@mail.com',
+                telefono: 83242834
+            },
+            valorMatricula: 200000
         },
-		valorMatricula: 200000
-      },
-    },
-    {
-      id: 2,
-      nombre: 'Pedro Perez',
-      detalles: {
-        descripcion: 'beca',
-        contacto: {
-          email: 'pperez@mail.com',
-		  telefono: 83242834
-        },
-		valorMatricula: 0
-      },
     },
     {
-      id: 3,
-      nombre: 'Juan Yarce',
-      detalles: {
-        descripcion: 'matricula normal',
-        contacto: {
-          value: 'jyarce@mail.com',
-		  telefono: 1122334455
+        id: 2,
+        nombre: 'Pedro Perez',
+        detalles: {
+            descripcion: 'beca',
+            contacto: {
+                email: 'pperez@mail.com',
+                telefono: 83242834
+            },
+            valorMatricula: 0
         },
-		valorMatricula: 400000
-      },
     },
-	{
-      id: 4,
-      nombre: 'Alberto Benitez',
-      detalles: {
-        descripcion: 'matricula condicional',
-        contacto: {
-          value: 'abenitez@mail.com',
-		  telefono: 234534566
+    {
+        id: 3,
+        nombre: 'Juan Yarce',
+        detalles: {
+            descripcion: 'matricula normal',
+            contacto: {
+                email: 'jyarce@mail.com',
+                telefono: 1122334455
+            },
+            valorMatricula: 400000
         },
-		valorMatricula: 600000
-      },
     },
-  ];
+    {
+        id: 4,
+        nombre: 'Alberto Benitez',
+        detalles: {
+            descripcion: 'matricula condicional',
+            contacto: {
+                email: 'abenitez@mail.com',
+                telefono: 234534566
+            },
+            valorMatricula: 600000
+        },
+    },
+];
 
-  const totalMaticulas = getNestedRecordsExample().reduce((total, item) => total + item.detalles.valorMatricula, 0);
-  document.write(`<h2>${totalMaticulas}</h2>`);
-  
+const totalMaticulas = `<h2>${getNestedRecordsExample().reduce((total, item) => total + item.detalles.valorMatricula, 0)}</h2>`;
+const telefonos = [];
+getNestedRecordsExample().forEach((e)=> telefonos.push(`<h2>${e.detalles.contacto.telefono}</h2>`));
+const estudianteMatriculaMax = getNestedRecordsExample().reduce((estudianteMasGrande, estudianteActual) => {
+    if (estudianteActual.detalles.valorMatricula > estudianteMasGrande.detalles.valorMatricula) {
+      return estudianteActual;
+    } else {
+      return estudianteMasGrande;
+    }
+  }, getNestedRecordsExample()[0]); 
+
+
+
+document.write(totalMaticulas, '<h1>Telefonos</h1>', telefonos, '<h1>Estudiante matricula mas grande</h1>', `<h2>${estudianteMatriculaMax.nombre} ${estudianteMatriculaMax.detalles.contacto.email}</h2>`);
